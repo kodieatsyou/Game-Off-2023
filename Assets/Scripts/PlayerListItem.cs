955e960f8786afd3b124d9ab37a2d694596c07f8
Patrick May <maypd@Patricks-MacBook-Pro.local>
2023-11-09 10:51:23 -0700

    Staging scene for lobby menu

---
+++ Assets/Scripts/PlayerListItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerListItem : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Text text;
+    Player player;
+
+    public void SetUp(Player _player)
+    {
+        player = _player;
+        text.text = _player.NickName;
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        if(player == otherPlayer)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public override void OnLeftRoom()
+    {
+        Destroy(gameObject);
+    }
+}
