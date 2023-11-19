75ab05037228d7aea67022009c83869c98f15267
Tanner May <13973912+kodieatsyou@users.noreply.github.com>
2023-11-19 02:05:03 -0700

    Die script revamped

--- Assets/Scripts/Menu/Lobby.cs
+++ Assets/Scripts/Menu/Lobby.cs
@@ -100,11 +100,11 @@ public class Lobby : MonoBehaviourPunCallbacks
     {
         if(string.IsNullOrEmpty(roomNameInput.text))
         {
-            PhotonNetwork.CreateRoom(GenerateRoomName(), new RoomOptions { MaxPlayers = maxPlayerInput.value+1, BroadcastPropsChangeToAll = true}, TypedLobby.Default);
+            PhotonNetwork.CreateRoom(GenerateRoomName(), new RoomOptions { MaxPlayers = int.Parse(maxPlayerInput.captionText.text), BroadcastPropsChangeToAll = true}, TypedLobby.Default);
         }
         else
         {
-            PhotonNetwork.CreateRoom(roomNameInput.text, new RoomOptions { MaxPlayers = maxPlayerInput.value+1, BroadcastPropsChangeToAll = true }, TypedLobby.Default);
+            PhotonNetwork.CreateRoom(roomNameInput.text, new RoomOptions { MaxPlayers = int.Parse(maxPlayerInput.captionText.text), BroadcastPropsChangeToAll = true }, TypedLobby.Default);
         }
         MenuManager.Instance.OpenMenu("loading");
     }
